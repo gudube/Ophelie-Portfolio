7b47a0ee77fc2402da9fdbfce2aa9691bae9f3ae
Guilhem Dubois <gdubois@prettyobjects.com>
2021-05-23 14:06:14 -0400

    hide arrow after scrolling

--- src/app/ui-tools/title-header/title-header.component.ts
+++ src/app/ui-tools/title-header/title-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-title-header',
@@ -12,12 +12,23 @@ export class TitleHeaderComponent implements OnInit {
 
 	@Input() public title = 'O P H E L I E';
 
+	public showDownArrow = true;
+
 	constructor() { }
 
 	ngOnInit(): void {
 	}
 
 	public scrollToBottom(): void {
-		window.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
+		if (this.showDownArrow) {
+			window.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
+		}
+	}
+
+	@HostListener('window:scroll', [])
+	private onWindowScroll(): void {
+		const currentScroll = window.pageYOffset || document.documentElement.scrollTop
+			|| document.body.scrollTop || 0;
+		this.showDownArrow = currentScroll < window.innerHeight / 4;
 	}
 }
