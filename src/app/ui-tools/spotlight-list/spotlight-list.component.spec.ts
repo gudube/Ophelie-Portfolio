155a9c99ff59b74a660caa112330d6c40d4349b6
Guilhem Dubois <gdubois@prettyobjects.com>
2021-06-07 18:44:06 -0400

    ajout debut chores. manque fleche pour scroll.

--- src/app/ui-tools/spotlight-list/spotlight-list.component.spec.ts
+++ src/app/ui-tools/spotlight-list/spotlight-list.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SpotlightListComponent } from './spotlight-list.component';
 
 describe('SpotlightListComponent', () => {
-  let component: SpotlightListComponent;
-  let fixture: ComponentFixture<SpotlightListComponent>;
+	let component: SpotlightListComponent;
+	let fixture: ComponentFixture<SpotlightListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SpotlightListComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [SpotlightListComponent],
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SpotlightListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(SpotlightListComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/ui-tools/spotlight-list/spotlight-list.component.ts
+++ src/app/ui-tools/spotlight-list/spotlight-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SpotlightModel } from '../spotlight-item/spotlight-model';
 
 @Component({
-  selector: 'app-spotlight-list',
-  templateUrl: './spotlight-list.component.html',
-  styleUrls: ['./spotlight-list.component.scss']
+	selector: 'app-spotlight-list',
+	templateUrl: './spotlight-list.component.html',
+	styleUrls: ['./spotlight-list.component.scss'],
 })
 export class SpotlightListComponent implements OnInit {
+	@Input() public items: SpotlightModel[] = [];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+	}
 }
