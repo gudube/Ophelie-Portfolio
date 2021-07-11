70a8cbe7ad8c277f1d64284e3efd347108081d18
Guilhem Dubois <gdubois@prettyobjects.com>
2021-07-11 16:44:52 -0400

    plein de changements

--- src/app/pages/page-peintures/page-peintures.component.ts
+++ src/app/pages/page-peintures/page-peintures.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { PhotoGridComponent } from 'src/app/ui-tools/photo-grid/photo-grid.component';
 import { PictureModel } from '../../ui-tools/photo-grid/picture-model';
 
 // eslint-disable-next-line no-shadow
@@ -10,6 +11,9 @@ enum Filter { None = '', Peintures = 'peintures/', Dessins = 'dessins/', Sculptu
 	styleUrls: ['./page-peintures.component.scss'],
 })
 export class PagePeinturesComponent implements OnInit {
+	@ViewChild('photoGrid')
+	public photoGrid!: ElementRef;
+
 	private drawings: PictureModel[] = [
 		{ fileName: 'peintures/p1.jpg', title: 'PORTAIT D’UN CIEL, UN JOUR QUI N’EXISTE PAS ENCORE', subtitle: '2014, acrylique sur toile, 76 x 61cm', description: 'description' },
 		{ fileName: 'peintures/p2.jpg', title: 'AU-DELÀ DU RÉEL', subtitle: '2018, acrylique sur toile, 51 x 40cm', description: 'description' },
@@ -20,6 +24,15 @@ export class PagePeinturesComponent implements OnInit {
 		{ fileName: 'peintures/p7.jpg', title: 'ESPÉRONS, AVANT QUE TOUT NE FONDE', subtitle: '2021, acrylique sur toile, 30.5 x 30.5cm', description: 'description' },
 		{ fileName: 'peintures/p8.jpg', title: 'ANNIE', subtitle: '2021, acrylique sur toile, 30.5 x 30.5cm', description: 'description' },
 		{ fileName: 'peintures/p9.jpg', title: 'JE VOUS LAISSE L’EXPLORER', subtitle: '2021, acrylique sur papier d’aquarelle, 212.5 x 122cm', description: 'description' },
+		{ fileName: 'dessins/d8.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d7.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d6.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d5.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d4.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d3.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d2.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d1.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
+		{ fileName: 'dessins/d0.jpg', title: 'titre 8', subtitle: 'sous-titre 8', description: 'description' },
 	];
 
 	public shownDrawings: PictureModel[] = [];
@@ -28,11 +41,28 @@ export class PagePeinturesComponent implements OnInit {
 
 	public currentFilter: Filter = Filter.None;
 
+	public currentTitle = '';
+
 	constructor() {
 	}
 
 	ngOnInit(): void {
+		// document.getElementById('presentation1')?.addEventListener('mouseover', () => {
+		// 	this.currentTitle = 'PEINTURES';
+		// });
 	}
+
+	// hoverImage(filter: Filter): void {
+	// 	if (filter === Filter.Peintures) {
+	// 		this.currentTitle = 'PEINTURES';
+	// 	} else if (filter === Filter.Sculptures) {
+	// 		this.currentTitle = 'SCULPTURES';
+	// 	} else if (filter === Filter.Dessins) {
+	// 		this.currentTitle = 'DESSINS';
+	// 	} else {
+	// 		this.currentTitle = '';
+	// 	}
+	// }
 
 	changeFilter(filter: Filter): void {
 		if (filter === this.currentFilter) {
@@ -44,5 +74,20 @@ export class PagePeinturesComponent implements OnInit {
 				filter.toString(),
 			));
 		}
+		if (this.currentFilter === Filter.Peintures) {
+			this.currentTitle = 'PEINTURES';
+		} else if (this.currentFilter === Filter.Sculptures) {
+			this.currentTitle = 'SCULPTURES';
+		} else if (this.currentFilter === Filter.Dessins) {
+			this.currentTitle = 'DESSINS';
+		} else {
+			this.currentTitle = '';
+		}
+
+		setTimeout(() => {
+			if (this.shownDrawings !== []) {
+				window.scrollTo({ top: document.getElementById('title-selected')?.offsetTop, behavior: 'smooth' });
+			}
+		}, 100);
 	}
 }
