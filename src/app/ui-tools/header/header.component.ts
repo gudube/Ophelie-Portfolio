3d3d2341d8803a7f512ff913e25c2df57a4ca9ea
Guilhem Dubois <gdubois@prettyobjects.com>
2021-08-20 12:30:12 -0400

    ajout barre orange sous titre

--- src/app/ui-tools/header/header.component.ts
+++ src/app/ui-tools/header/header.component.ts
@@ -1,13 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
+	@ViewChild('onglets')
+	ongletsContainer!: any;
+
+	public indicatorPos = 0;
+
+	public visibleIndicator = false;
+
 	constructor() { }
 
-	ngOnInit(): void {
+	ngAfterViewInit(): void {
+		setTimeout(() => {
+			this.computeIndicatorPos();
+		}, 0);
+	}
+
+	private computeIndicatorPos(): void {
+		const activeOnglets = this.ongletsContainer.nativeElement.getElementsByClassName('active');
+		if (activeOnglets.length !== 1 || !activeOnglets[0]) {
+			this.visibleIndicator = false;
+		} else {
+			const activeOnglet = activeOnglets[0] as HTMLElement;
+			console.log(this.ongletsContainer.nativeElement.clientLeft);
+			this.indicatorPos = activeOnglet.offsetLeft + activeOnglet.clientWidth / 2;
+			this.visibleIndicator = true;
+		}
 	}
 }
