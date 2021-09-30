80a4e6b98447986a4ba2373597e38282fc3109b0
Guilhem Dubois <gdubois@prettyobjects.com>
2021-09-30 17:43:07 -0400

    modification page accueil

--- src/app/pages/page-about/page-about.component.ts
+++ src/app/pages/page-about/page-about.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+// eslint-disable-next-line no-shadow
+// enum Page { None = '', Bio = 'peintures/', Dem = 'dessins/', Exp = 'sculptures/' }
+
 @Component({
-  selector: 'app-page-about',
-  templateUrl: './page-about.component.html',
-  styleUrls: ['./page-about.component.scss']
+	selector: 'app-page-about',
+	templateUrl: './page-about.component.html',
+	styleUrls: ['./page-about.component.scss'],
 })
 export class PageAboutComponent implements OnInit {
+	constructor() { }
 
-  constructor() { }
+	ngOnInit(): void {
+	}
 
-  ngOnInit(): void {
-  }
+	// public pages = Page;
 
+	// public hoverTitle: string | null = null;
+
+	// hoverImage(filter: Page): void {
+	// 	if (filter === Page.Bio) {
+	// 		this.hoverTitle = 'BIOGRAPHIE';
+	// 	} else if (filter === Page.Dem) {
+	// 		this.hoverTitle = 'DEMARCHE ARTISTIQUE';
+	// 	} else if (filter === Page.Exp) {
+	// 		this.hoverTitle = 'EXPERIENCES';
+	// 	} else {
+	// 		this.hoverTitle = null;
+	// 	}
+	// }
+
+	// unhoverImage(): void {
+	// 	this.hoverTitle = null;
+	// }
 }
