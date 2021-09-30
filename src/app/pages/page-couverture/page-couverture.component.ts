85cd8d3bf3c80f3e07a43928324ab48ffafe546e
Guilhem Dubois <gdubois@prettyobjects.com>
2021-09-30 16:15:49 -0400

    ease out anim couverture

--- src/app/pages/page-couverture/page-couverture.component.ts
+++ src/app/pages/page-couverture/page-couverture.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef, Component, OnInit, ViewChild } from '@angular/core';
+
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-page-couverture',
-  templateUrl: './page-couverture.component.html',
-  styleUrls: ['./page-couverture.component.scss']
+	selector: 'app-page-couverture',
+	templateUrl: './page-couverture.component.html',
+	styleUrls: ['./page-couverture.component.scss'],
 })
 export class PageCouvertureComponent implements OnInit {
+	@ViewChild('bgImage') bgImage!: ElementRef;
 
-  constructor() { }
+	@ViewChild('enterButton') enterButton!: ElementRef;
 
-  ngOnInit(): void {
-  }
+	constructor(private router: Router) { }
 
+	ngOnInit(): void {
+	}
+
+	public enter(): void {
+		this.enterButton.nativeElement.classList.add('disappear');
+		this.bgImage.nativeElement.classList.add('disappear');
+
+		setTimeout(() => {
+			this.router.navigate(['/accueil']);
+		}, 200);
+	}
 }
